038f1dcd9cb1a27f1b6d41a0f5c1925c1291aff1
KostyaYamshanov <k.yamshanov@fastsense.tech>
2021-09-22 12:01:36 +0700

    add some docstrings

--- mppic/test/tests_utils.hpp
+++ mppic/test/tests_utils.hpp
@@ -1,4 +1,6 @@
 
+#pragma once
+
 #include <iostream>
 #include <algorithm>
 #include "nav2_costmap_2d/costmap_2d_ros.hpp"
@@ -6,6 +8,11 @@
 #include <rclcpp/executors.hpp>
 
 void printMap(const nav2_costmap_2d::Costmap2D & costmap){
+/**
+ * Print costmap to stdout.
+ *
+ * @param costmap map to be printed.
+*/
   for (unsigned int i = 0; i < costmap.getSizeInCellsY(); i++) {
     for (unsigned int j = 0; j < costmap.getSizeInCellsX(); j++) {
       printf("%4d", static_cast<int>(costmap.getCost(j, i)));
@@ -17,21 +24,30 @@ void printMap(const nav2_costmap_2d::Costmap2D & costmap){
 
 void printMapWthGoalAndTrajectory(nav2_costmap_2d::Costmap2D & costmap, const auto & trajectory, 
     const geometry_msgs::msg::PoseStamped & goal_point){
-  printf("map with trajectory: \ntrajectory point = 1 \ngoal point = 2 \nobsctacle = 255\n");
+/**
+ * Print costmap with trajectory and goal point to stdout.
+ *
+ * @param costmap map to be printed.
+ * @param trajectory trajectory container (xt::tensor) to be printed.
+ * @param goal_point goal point to be printed.
+*/
+  printf("map with trajectory: \ntrajectory point = 1 \ngoal point = 8 \nobsctacle = 255\n");
 
   // create new costmap
   nav2_costmap_2d::Costmap2D costmap2d(
-      costmap.getSizeInCellsX(),
-      costmap.getSizeInCellsY(),
-      costmap.getResolution(),
-      costmap.getOriginX(),
-      costmap.getOriginY(),
-      costmap.getDefaultValue());
+    costmap.getSizeInCellsX(),
+    costmap.getSizeInCellsY(),
+    costmap.getResolution(),
+    costmap.getOriginX(),
+    costmap.getOriginY(),
+    costmap.getDefaultValue()
+  );
 
   // copy obstacles from original costmap
   costmap2d = costmap;
   unsigned int point_mx, point_my;
   unsigned char trajectory_point = 1;
+  unsigned char goal_point_cost = 8;
 
   // add trajectory on map
   for (auto i = trajectory.begin(); i < trajectory.end(); i+=3){
@@ -44,7 +60,7 @@ void printMapWthGoalAndTrajectory(nav2_costmap_2d::Costmap2D & costmap, const au
         goal_point.pose.position.y,
         point_mx, point_my);
     
-    costmap2d.setCost(point_mx, point_my, 2);
+    costmap2d.setCost(point_mx, point_my, goal_point_cost);
 
     printMap(costmap2d);
 
@@ -52,6 +68,11 @@ void printMapWthGoalAndTrajectory(nav2_costmap_2d::Costmap2D & costmap, const au
 
 
 void setUpOptimizerParams(std::vector<rclcpp::Parameter> &params_){
+/**
+ * Adds some parameters for the optimizer to a special container.
+ *
+ * @param params_ container for optimizer's parameters.
+*/
   params_.push_back(rclcpp::Parameter("TestNode.iteration_count", 300));
   params_.push_back(rclcpp::Parameter("TestNode.lookahead_dist", 5));
   params_.push_back(rclcpp::Parameter("TestNode.time_steps", 60));
@@ -59,6 +80,15 @@ void setUpOptimizerParams(std::vector<rclcpp::Parameter> &params_){
 
 void addObstacle(nav2_costmap_2d::Costmap2D & costmap, const unsigned int & mx, 
   const unsigned int & my, const unsigned int & size, const unsigned char & cost){
+/**
+ * Adds a square obstacle to the costmap.
+ *
+ * @param costmap map to be modified.
+ * @param mx obstacle upper left corner X coord (on the costmap).
+ * @param my obstacle upper left corner Y coord (on the costmap).
+ * @param size obstacle side size.
+ * @param cost obstacle value on costmap.
+*/
 
   for (unsigned int i = mx; i < mx+size; i++) {
     for (unsigned int j = my; j < my+size; j++) {
@@ -68,7 +98,14 @@ void addObstacle(nav2_costmap_2d::Costmap2D & costmap, const unsigned int & mx,
 }
 
 bool checkTrajectoryCollision(const auto & trajectory, const nav2_costmap_2d::Costmap2D & costmap){
-  
+/**
+ * Checks the trajectory for collisions with obstacles on the map.
+ *
+ * @param trajectory trajectory container (xt::tensor) to be checked.
+ * @param costmap costmap with obstacles
+ * 
+ * @return true - if the trajectory crosses an obstacle on the map, false - if not
+*/
   unsigned int point_mx, point_my;
   for (auto i = trajectory.begin(); i < trajectory.end(); i+=3){
     costmap.worldToMap(*i, *(i+1), point_mx, point_my);
