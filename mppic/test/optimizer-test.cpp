8325009677a76bbc2307b579d8560d0c87919ff7
KostyaYamshanov <k.yamshanov@fastsense.tech>
2021-09-22 10:42:33 +0700

    add some comments to the code

--- mppic/test/optimizer-test.cpp
+++ mppic/test/optimizer-test.cpp
@@ -87,37 +87,63 @@ TEST_CASE("Optimizer evaluates Next Control", "") {
 
 
 
-TEST_CASE("Optimizer with obstacles", "") {
+TEST_CASE("Optimizer with costmap2d and obstacles", "[collision]") {
 
   using T = float;
 
   std::string node_name = "TestNode";
+  auto st = rclcpp_lifecycle::State{};
   std::vector<rclcpp::Parameter> params_;
   rclcpp::NodeOptions options;
   setUpOptimizerParams(params_);
   options.parameter_overrides(params_);
 
+  // args for costmap2d
+  unsigned int cells_size_x = 30;
+  unsigned int cells_size_y = 30;
+  double resolution = 0.1;
+  double origin_x = 0.0;
+  double origin_y = 0.0;
+  unsigned char default_value = 0;
+  
+  // args for obstacle on costmap2d
+  const unsigned int upper_left_corner_x = 5;  // x coord of the upper left corner of the obstacle on costmap
+  const unsigned int upper_left_corner_y = 5;  // upper left corner of the obstacle on costmap
+  const unsigned int obstacle_side_size = 18;  // in cells
+  unsigned char cost = 255;                    // value of the obstacle on costmap                  
+  
+  // create parameters for reference path generation
+  size_t poses_count = 100; // it can be GENERATE(100)
+  float x_step = 0.025;
+  float y_step = 0.01;
+
   auto node = std::make_shared<rclcpp_lifecycle::LifecycleNode>(node_name, options);
-
   auto costmap_ros = std::make_shared<nav2_costmap_2d::Costmap2DROS>("cost_map_node");
-  auto st = rclcpp_lifecycle::State{};
-
+  
   auto & model = mppi::models::NaiveModel<T>;
-
   auto optimizer = mppi::optimization::Optimizer<T>();
 
   costmap_ros->on_configure(st);
-  *costmap_ros->getCostmap() = *std::make_shared<nav2_costmap_2d::Costmap2D>(30, 30, 0.1, 0, 0, 0);
-  addObstacle(*costmap_ros->getCostmap(), 5, 5, 18, 255);
+  *costmap_ros->getCostmap() = *std::make_shared<nav2_costmap_2d::Costmap2D>(
+    cells_size_x, 
+    cells_size_y,
+    resolution,
+    origin_x,
+    origin_y,
+    default_value
+  );
+
+  // creating a square obstacle
+  addObstacle(*costmap_ros->getCostmap(), upper_left_corner_x, 
+              upper_left_corner_y, obstacle_side_size, cost);
+
   optimizer.on_configure(node, node_name, costmap_ros, model);
   optimizer.on_activate();
 
-  size_t poses_count = 100;
-
   SECTION("Running evalTrajectoryFromControlSequence") {
     
-    std::string frame = "odom";
-    auto time = node->get_clock()->now();
+    std::string frame = "odom";                // frame for header in path
+    auto time = node->get_clock()->now();      // time for header in path
 
     nav_msgs::msg::Path path;                  // reference path
     geometry_msgs::msg::PoseStamped ps;        // goal point in reference path
@@ -133,8 +159,8 @@ TEST_CASE("Optimizer with obstacles", "") {
     // lambda expression for refernce path generation
     auto fillRealPath = [&](size_t count) {
       for (size_t i = 0; i < count; i++) {
-          ps.pose.position.x = i*0.025;
-          ps.pose.position.y = i*0.01;
+          ps.pose.position.x = i*x_step;
+          ps.pose.position.y = i*y_step;
           path.poses.push_back(ps);
       }
     };  
@@ -145,9 +171,11 @@ TEST_CASE("Optimizer with obstacles", "") {
 
     fillRealPath(poses_count);
 
+    // update controal sequence in optimizer
     CHECK_NOTHROW(optimizer.evalNextBestControl(init_cond, twist, path));
-    
+    // get best trajectory from optimizer
     auto trajectory = optimizer.evalTrajectoryFromControlSequence(init_cond, twist);
+    // check trajectory for collision
     bool result = checkTrajectoryCollision(trajectory, *costmap_ros->getCostmap());
 #ifdef PRINT_COSTMAP
 printMapWthGoalAndTrajectory(*costmap_ros->getCostmap(), trajectory, ps);
