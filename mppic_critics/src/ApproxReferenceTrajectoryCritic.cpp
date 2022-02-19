c5ebc20fbb231ae706234f5e06103be33491312f
artofnothingness <speedy_sky@mail.ru>
2022-02-19 14:02:38 +0300

    temp

---
+++ mppic_critics/src/ApproxReferenceTrajectoryCritic.cpp
@@ -0,0 +1,10 @@
+#include "mppic_critics/ApproxReferenceTrajectoryCritic.hpp"
+
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(
+  mppi::optimization::ApproxReferenceTrajectoryCritic<float>,
+  mppi::optimization::CriticFunction<float>)
+PLUGINLIB_EXPORT_CLASS(
+  mppi::optimization::ApproxReferenceTrajectoryCritic<double>,
+  mppi::optimization::CriticFunction<double>)
