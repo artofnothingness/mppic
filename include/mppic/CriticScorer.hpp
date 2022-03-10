77fafb39ace4de8af36560cd4a643a155d3a2e6c
Steve Macenski <stevenmacenski@gmail.com>
2022-03-09 16:26:11 -0800

    migrating scorers as big component to main algorithm

---
+++ include/mppic/CriticScorer.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <pluginlib/class_loader.hpp>
+#include <xtensor/xtensor.hpp>
+
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+
+
+#include "mppic/CriticFunction.hpp"
+#include "mppic/utils.hpp"
+
+namespace mppi::optimization {
+
+template <typename T>
+class CriticScorer
+{
+public:
+  CriticScorer() = default;
+
+  void on_configure(
+    rclcpp_lifecycle::LifecycleNode::WeakPtr parent,
+    const std::string & node_name,
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros);
+
+  void setLoader();
+
+  std::string getFullName(const std::string & name);
+
+  void getParams();
+
+  void loadCritics();
+
+  void configureCritics();
+
+  /**
+   * @brief Evaluate cost for each trajectory
+   *
+   * @param trajectories: tensor of shape [ ..., ..., 3 ]
+   * where 3 stands for x, y, yaw
+   * @return Cost for each trajectory
+   */
+  xt::xtensor<T, 1> evalTrajectoriesScores(
+    const xt::xtensor<T, 3> & trajectories, const nav_msgs::msg::Path & global_plan,
+    const geometry_msgs::msg::PoseStamped & robot_pose) const;
+
+protected:
+  rclcpp_lifecycle::LifecycleNode::WeakPtr parent_;
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+  std::string node_name_;
+
+  std::vector<std::string> critics_names_;
+  std::string critics_type_;
+  const std::string base_name_ = "CriticFunction";
+
+  std::unique_ptr<pluginlib::ClassLoader<optimization::CriticFunction<T>>> loader_;
+  std::vector<std::unique_ptr<CriticFunction<T>>> critics_;
+
+  rclcpp::Logger logger_{rclcpp::get_logger("MPPI CriticScorer")};
+};
+
+} // namespace mppi::optimization
---
+++ src/CriticScorer.cpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "mppic/CriticScorer.hpp"
+
+namespace mppi::optimization {
+
+template<typename T>
+void CriticScorer<T>::on_configure(
+  rclcpp_lifecycle::LifecycleNode::WeakPtr parent,
+  const std::string & node_name,
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
+{
+  parent_ = parent;
+  costmap_ros_ = costmap_ros;
+  node_name_ = node_name;
+
+  getParams();
+  setLoader();
+  loadCritics();
+  configureCritics();
+
+  RCLCPP_INFO(logger_, "Configured");
+}
+
+template<typename T>
+void CriticScorer<T>::setLoader()
+{
+  if (!loader_) {
+    loader_ = std::make_unique<pluginlib::ClassLoader<optimization::CriticFunction<T>>>(
+      "mppic", getFullName(base_name_));
+  }
+}
+
+template<typename T>
+std::string CriticScorer<T>::getFullName(const std::string & name)
+{
+  return "mppi::optimization::" + name + "<" + critics_type_ + ">";
+}
+
+template<typename T>
+void CriticScorer<T>::getParams()
+{
+  auto node = parent_.lock();
+  auto getParam = utils::getParamGetter(node, node_name_);
+  getParam(critics_names_, "critics_names", std::vector<std::string>{});
+  getParam(critics_type_, "critics_type", std::string("float"));
+}
+
+template<typename T>
+void CriticScorer<T>::loadCritics()
+{
+  critics_.clear();
+  for (auto name : critics_names_) {
+    std::string fullname = getFullName(name);
+    auto instance = std::unique_ptr<optimization::CriticFunction<T>>(
+      loader_->createUnmanagedInstance(fullname));
+    critics_.push_back(std::move(instance));
+    RCLCPP_INFO(logger_, "Critic loaded : %s", fullname.c_str());
+  }
+}
+
+template<typename T>
+void CriticScorer<T>::configureCritics()
+{
+  for (size_t q = 0; q < critics_.size(); q++) {
+    critics_[q]->on_configure(parent_, node_name_ + "." + critics_names_[q], costmap_ros_);
+  }
+}
+
+template<typename T>
+xt::xtensor<T, 1> CriticScorer<T>::evalTrajectoriesScores(
+  const xt::xtensor<T, 3> & trajectories, const nav_msgs::msg::Path & global_plan,
+  const geometry_msgs::msg::PoseStamped & robot_pose) const
+{
+  size_t trajectories_count = trajectories.shape()[0];
+  xt::xtensor<T, 1> costs = xt::zeros<T>({trajectories_count});
+
+  if (global_plan.poses.empty()) {
+    return costs;
+  }
+
+  xt::xtensor<T, 2> path = std::move(utils::toTensor<T>(global_plan));
+
+  for (size_t q = 0; q < critics_.size(); q++) {
+    critics_[q]->score(robot_pose, trajectories, path, costs);
+  }
+
+  return costs;
+}
+
+} // namespace mppi::optimization
