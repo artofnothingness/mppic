200255723c632210a93b0983d0f3634418f1c44a
artofnothingness <speedy_sky@mail.ru>
2022-03-11 01:52:27 +0300

    critics untemplated

--- src/critics/angle_to_goal_critic.cpp
+++ src/critics/angle_to_goal_critic.cpp
@@ -1,8 +1,5 @@
-#include "mppic/optimization/scoring/critics/angle_to_goal_critic.hpp"
+#include "mppic/critics/angle_to_goal_critic.hpp"
 
 #include <pluginlib/class_list_macros.hpp>
 
-PLUGINLIB_EXPORT_CLASS(
-  mppi::optimization::AngleToGoalCritic<float>, mppi::optimization::CriticFunction<float>)
-PLUGINLIB_EXPORT_CLASS(
-  mppi::optimization::AngleToGoalCritic<double>, mppi::optimization::CriticFunction<double>)
+PLUGINLIB_EXPORT_CLASS(mppi::optimization::AngleToGoalCritic, mppi::optimization::CriticFunction)
