cbfe0136d32af0fcafbcbbabb731001827cca280
Alex <48126993+artofnothingness@users.noreply.github.com>
2022-03-11 20:43:17 +0300

    Update goal_angle_critic.hpp

--- include/mppic/critics/goal_angle_critic.hpp
+++ include/mppic/critics/goal_angle_critic.hpp
@@ -35,7 +35,7 @@ public:
    * @param costs [out] add goal angle cost values to this tensor
    */
   virtual void score(
-    const geometry_msgs::msg::PoseStamped & /*robot_pose*/, const xt::xtensor<double, 3> & trajectories,
+    const geometry_msgs::msg::PoseStamped & robot_pose, const xt::xtensor<double, 3> & trajectories,
     const xt::xtensor<double, 2> & path, xt::xtensor<double, 1> & costs) override
   {
     xt::xtensor<double, 1> tensor_pose = {
