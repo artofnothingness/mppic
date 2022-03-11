f660d58b1c161363f167fbb234df59513d556cfe
Steve Macenski <stevenmacenski@gmail.com>
2022-03-10 17:23:39 -0800

    rename node_name to name, since its actually the plugin name, not the node

--- include/mppic/critics/reference_trajectory_critic.hpp
+++ include/mppic/critics/reference_trajectory_critic.hpp
@@ -13,13 +13,13 @@ class ReferenceTrajectoryCritic : public CriticFunction<T>
 {
 public:
   using CriticFunction<T>::parent_;
-  using CriticFunction<T>::node_name_;
+  using CriticFunction<T>::name_;
   using CriticFunction<T>::logger_;
 
   void getParams() override
   {
     auto node = parent_.lock();
-    auto getParam = utils::getParamGetter(node, node_name_);
+    auto getParam = utils::getParamGetter(node, name_);
     getParam(power_, "reference_cost_power", 1);
     getParam(weight_, "reference_cost_weight", 15);
     RCLCPP_INFO(
