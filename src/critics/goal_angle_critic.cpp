9d6b2ab4ff6fa31d935f85f73b8a1a8af50cfd37
Steve Macenski <stevenmacenski@gmail.com>
2022-03-16 16:59:18 -0700

    use goal tolerance to disable critics not of interest to within XY tolerance

--- src/critics/goal_angle_critic.cpp
+++ src/critics/goal_angle_critic.cpp
@@ -19,7 +19,8 @@ void GoalAngleCritic::initialize()
 
 void GoalAngleCritic::score(
   const geometry_msgs::msg::PoseStamped & robot_pose, const xt::xtensor<double, 3> & trajectories,
-  const xt::xtensor<double, 2> & path, xt::xtensor<double, 1> & costs)
+  const xt::xtensor<double, 2> & path, xt::xtensor<double, 1> & costs,
+  nav2_core::GoalChecker * /*goal_checker*/)
 {
   xt::xtensor<double, 1> tensor_pose = {
     static_cast<double>(robot_pose.pose.position.x),
