2e80fbbc392767bdcd09b298ef37139641605527
artofnothingness <speedy_sky@mail.ru>
2022-04-16 16:25:15 +0300

    revert + rework param handler

--- include/mppic/parameters_handler.hpp
+++ include/mppic/parameters_handler.hpp
@@ -2,25 +2,27 @@
 
 #pragma once
 
-#include <vector>
-#include <utility>
-#include <string>
-#include <unordered_map>
 #include <functional>
+#include <rclcpp/parameter_value.hpp>
+#include <string>
 #include <type_traits>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
+#include "nav2_util/node_utils.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
-#include "nav2_util/node_utils.hpp"
 
 namespace mppi
 {
 
-enum class ParameterType
+enum class ParameterType { Dynamic, Static };
+
+namespace
 {
-  Dynamic,
-  Static
-};
+
+}  // namespace
 
 class ParametersHandler
 {
@@ -30,137 +32,150 @@ public:
   using pre_callback_t = void ();
 
   ParametersHandler() = default;
-  explicit ParametersHandler(const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent)
-  {
-    node_ = parent;
-    auto node = node_.lock();
-    logger_ = node->get_logger();
-  }
+  explicit ParametersHandler(
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent);
 
-  void start()
-  {
-    auto node = node_.lock();
-    on_set_param_handler_ = node->add_on_set_parameters_callback(
-      std::bind(
-        &ParametersHandler::dynamicParametersCallback, this, std::placeholders::_1));
-  }
+  void start();
 
   rcl_interfaces::msg::SetParametersResult dynamicParametersCallback(
-    std::vector<rclcpp::Parameter> parameters)
-  {
-    rcl_interfaces::msg::SetParametersResult result;
+    std::vector<rclcpp::Parameter> parameters);
 
-    for (auto & pre_cb : pre_callbacks_) {
-      pre_cb();
-    }
-
-    for (auto & param : parameters) {
-      const std::string & param_name = param.get_name();
-
-      if (auto callback = get_param_callbacks_.find(param_name);
-        callback != get_param_callbacks_.end())
-      {
-        RCLCPP_INFO(logger_, "Parameter %s found", param_name.c_str());
-        callback->second(param);
-      } else {
-        RCLCPP_WARN(logger_, "Parameter %s not found", param_name.c_str());
-      }
-    }
-
-    for (auto & post_cb : post_callbacks_) {
-      post_cb();
-    }
-
-    result.successful = true;
-    return result;
-  }
-
-  template<typename T>
-  void addPostCallback(T && callback)
-  {
-    post_callbacks_.push_back(std::move(callback));
-  }
-
-  template<typename T>
-  void addPreCallback(T && callback)
-  {
-    pre_callbacks_.push_back(std::move(callback));
-  }
-
-  auto getParamGetter(std::string ns)
-  {
-    return [this, ns = std::move(ns)](auto & setting, std::string name, auto default_value,
-             ParameterType param_type = ParameterType::Dynamic) {
-             getParam(
-               setting, ns == "" ? name : ns + "." + name, std::move(
-                 default_value), param_type);
-           };
-  }
+  inline auto getParamGetter(const std::string &ns);
 
   template<typename SettingT, typename ParamT>
   void getParam(
-    SettingT & setting, std::string name, ParamT default_value,
-    ParameterType param_type = ParameterType::Dynamic)
-  {
-    auto node = node_.lock();
-
-    nav2_util::declare_parameter_if_not_declared(
-      node, name, rclcpp::ParameterValue(default_value));
-
-    setParameter<ParamT>(node, setting, name);
-
-    if (param_type == ParameterType::Dynamic) {
-      setDynamicParamCallback(setting, name);
-    }
-  }
-
-  template<typename ParamT, typename SettingT, typename NodeT>
-  void setParameter(NodeT node, SettingT & setting, std::string name)
-  {
-    ParamT param_in;
-    node->get_parameter(name, param_in);
-    setting = static_cast<SettingT>(param_in);
-  }
+    SettingT & setting, const std::string &name, ParamT default_value,
+    ParameterType param_type = ParameterType::Dynamic);
 
   template<typename T>
-  void setDynamicParamCallback(
-    T & setting, std::string name)
-  {
-    if (auto it = get_param_callbacks_.find(name);
-      it != get_param_callbacks_.end())
-    {
-      return;
-    }
+  void addPostCallback(T && callback);
 
-    get_param_callbacks_[name] = [this, &setting, name](const rclcpp::Parameter & param) {
-        using setting_t = T;
+  template<typename T>
+  void addPreCallback(T && callback);
 
-        if constexpr (std::is_same_v<setting_t, bool>) {
-          auto param_value = param.as_bool();
-          setting = param_value;
-          RCLCPP_INFO(logger_, "Parameter %s set to %d", name.c_str(), param_value);
-        } else if constexpr (std::is_integral_v<setting_t>) {
-          auto param_value = param.as_int();
-          setting = param_value;
-          RCLCPP_INFO(logger_, "Parameter %s set to %ld", name.c_str(), param_value);
-        } else if constexpr (std::is_floating_point_v<setting_t>) {
-          auto param_value = param.as_double();
-          setting = param_value;
-          RCLCPP_INFO(logger_, "Parameter %s set to %f", name.c_str(), param_value);
-        }
-      };
+  template<typename ParamT, typename SettingT, typename NodeT>
+  void setParameter(SettingT & setting, const std::string &name, NodeT node);
 
-    RCLCPP_INFO(logger_, "Dynamic Parameter added %s", name.c_str());
-  }
+  template<typename T>
+  void setDynamicParamCallback(T & setting, const std::string &name);
 
 private:
+  template<typename T>
+  void addDynamicParameterCallback(const std::string & name, T && callback);
+
+  template<typename T>
+  static auto as(const rclcpp::Parameter & parameter)
+  {
+    if constexpr (std::is_same_v<T, bool>) {
+      return parameter.as_bool();
+    } else if constexpr (std::is_integral_v<T>) {
+      return parameter.as_int();
+    } else if constexpr (std::is_floating_point_v<T>) {
+      return parameter.as_double();
+    } else if constexpr (std::is_same_v<T, std::string>) {
+      return parameter.as_string();
+    } else if constexpr (std::is_same_v<T, std::vector<int>>) {
+      return parameter.as_integer_array();
+    } else if constexpr (std::is_same_v<T, std::vector<int64_t>>) {
+      return parameter.as_integer_array();
+    } else if constexpr (std::is_same_v<T, std::vector<unsigned int>>) {
+      return parameter.as_integer_array();
+    } else if constexpr (std::is_same_v<T, std::vector<size_t>>) {
+      return parameter.as_integer_array();
+    } else if constexpr (std::is_same_v<T, std::vector<double>>) {
+      return parameter.as_double_array();
+    } else if constexpr (std::is_same_v<T, std::vector<float>>) {
+      return parameter.as_double_array();
+    } else if constexpr (std::is_same_v<T, std::vector<std::string>>) {
+      return parameter.as_string_array();
+    }
+  }
+
   rclcpp::Logger logger_{rclcpp::get_logger("MPPIController")};
-  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr on_set_param_handler_;
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr
+    on_set_param_handler_;
   rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
-  std::unordered_map<std::string, std::function<get_param_func_t>> get_param_callbacks_;
-  std::vector<std::function<post_callback_t>> post_callbacks_;
+
+  std::unordered_map<std::string, std::function<get_param_func_t>>
+  get_param_callbacks_;
+
   std::vector<std::function<pre_callback_t>> pre_callbacks_;
+  std::vector<std::function<post_callback_t>> post_callbacks_;
 };
 
+inline auto ParametersHandler::getParamGetter(const std::string &ns)
+{
+  return [this, ns](
+    auto & setting, const std::string &name, auto default_value,
+    ParameterType param_type = ParameterType::Dynamic) {
+           getParam(
+             setting, ns == "" ? name : ns + "." + name,
+             std::move(default_value), param_type);
+         };
+}
+
+
+template<typename T>
+void ParametersHandler::addDynamicParameterCallback(const std::string & name, T && callback)
+{
+  get_param_callbacks_[name] = std::move(callback);
+}
+
+template<typename T>
+void ParametersHandler::addPostCallback(T && callback)
+{
+  post_callbacks_.push_back(std::move(callback));
+}
+
+template<typename T>
+void ParametersHandler::addPreCallback(T && callback)
+{
+  pre_callbacks_.push_back(std::move(callback));
+}
+
+template<typename SettingT, typename ParamT>
+void ParametersHandler::getParam(
+  SettingT & setting, const std::string &name,
+  ParamT default_value,
+  ParameterType param_type)
+{
+  auto node = node_.lock();
+
+  nav2_util::declare_parameter_if_not_declared(
+    node, name, rclcpp::ParameterValue(default_value));
+
+  setParameter<ParamT>(setting, name, node);
+
+  if (param_type == ParameterType::Dynamic) {
+    setDynamicParamCallback(setting, name);
+  }
+}
+
+template<typename ParamT, typename SettingT, typename NodeT>
+void ParametersHandler::setParameter(
+  SettingT & setting, const std::string &name, NodeT node)
+{
+  ParamT param_in;
+  node->get_parameter(name, param_in);
+  setting = static_cast<SettingT>(param_in);
+}
+
+template<typename T>
+void ParametersHandler::setDynamicParamCallback(T & setting, const std::string &name)
+{
+  if (get_param_callbacks_.find(name) != get_param_callbacks_.end()) {
+    return;
+  }
+
+  auto callback = [this, &setting, name](const rclcpp::Parameter & param) {
+      auto param_value = as<T>(param);
+      setting = param_value;
+      RCLCPP_INFO(logger_, "Dynamic parameter changed: %s", std::to_string(param).c_str());
+    };
+
+  addDynamicParameterCallback(name, callback);
+
+  RCLCPP_INFO(logger_, "Dynamic Parameter added %s", name.c_str());
+}
 
 }  // namespace mppi
---
+++ src/parameters_handler.cpp
@@ -0,0 +1,53 @@
+#include "mppic/parameters_handler.hpp"
+
+namespace mppi
+{
+
+ParametersHandler::ParametersHandler(
+  const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent)
+{
+  node_ = parent;
+  auto node = node_.lock();
+  logger_ = node->get_logger();
+}
+
+void ParametersHandler::start()
+{
+  auto node = node_.lock();
+  on_set_param_handler_ = node->add_on_set_parameters_callback(
+    std::bind(
+      &ParametersHandler::dynamicParametersCallback, this,
+      std::placeholders::_1));
+}
+
+rcl_interfaces::msg::SetParametersResult
+ParametersHandler::dynamicParametersCallback(
+  std::vector<rclcpp::Parameter> parameters)
+{
+  rcl_interfaces::msg::SetParametersResult result;
+
+  for (auto & pre_cb : pre_callbacks_) {
+    pre_cb();
+  }
+
+  for (auto & param : parameters) {
+    const std::string & param_name = param.get_name();
+
+    if (auto callback = get_param_callbacks_.find(param_name);
+      callback != get_param_callbacks_.end())
+    {
+      callback->second(param);
+    } else {
+      RCLCPP_WARN(logger_, "Parameter %s not found", param_name.c_str());
+    }
+  }
+
+  for (auto & post_cb : post_callbacks_) {
+    post_cb();
+  }
+
+  result.successful = true;
+  return result;
+}
+
+}  // namespace mppi
